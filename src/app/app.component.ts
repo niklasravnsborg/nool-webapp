eaa7e1fdd5920fce771bfd36c0aa867f9900d912
Niklas Ravnsborg-Gjertsen <niklasravnsborg@gmail.com>
2016-12-08 16:22:22 +0100

    feat: add basic functionality for admins

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFire, FirebaseListObservable, AuthProviders, AuthMethods, FirebaseAuth } from 'angularfire2';
+import { Ng2UploaderModule } from 'ng2-uploader';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,67 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  constructor(public auth: FirebaseAuth, public af: AngularFire) { }
+
+  items: FirebaseListObservable<any[]>;
+  user = {email: '', password: ''};
+  newItem = '';
+  loggedIn = false;
+
+  uploadFile: any;
+  hasBaseDropZoneOver: boolean = false;
+  options: Object = {
+    url: 'http://nool.niklasravnsborg.com/parse/'
+  };
+  sizeLimit = 2000000;
+
+  handleUpload(data): void {
+    if (data && data.response) {
+      data = JSON.parse(data.response);
+      this.uploadFile = data;
+      for (let message of data) {
+        this.items.push(message);
+      }
+
+    }
+  }
+
+  fileOverBase(e:any):void {
+    this.hasBaseDropZoneOver = e;
+  }
+
+  beforeUpload(uploadingFile): void {
+    if (uploadingFile.size > this.sizeLimit) {
+      uploadingFile.setAbort();
+      alert('File is too large');
+    }
+  }
+
+  login() {
+    this.auth.login(this.user, {
+      provider: AuthProviders.Password,
+      method: AuthMethods.Password
+    }).then((authData) => {
+      // this.loader.dismiss();
+      this.items = this.af.database.list('/messages');
+      this.loggedIn = true;
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
+  addItem() {
+    this.items.push({ course: this.newItem, type: 'canceled'});
+    this.newItem = '';
+  }
+
+  removeItem(item) {
+    console.log(item);
+    this.items.remove(item);
+  }
+
+  logout() {
+     this.af.auth.logout();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,22 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
+import { Ng2UploaderModule } from 'ng2-uploader';
+
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyB6BmmDaCCC0UEWQ_yqfvojXIeOFOxM3J0',
+  authDomain: 'nool-b1267.firebaseapp.com',
+  databaseURL: 'https://nool-b1267.firebaseio.com',
+  storageBucket: 'nool-b1267.appspot.com'
+};
+
+const firebaseAuthConfig = {
+  provider: AuthProviders.Password,
+  method: AuthMethods.Redirect
+}
 
 @NgModule({
   declarations: [
@@ -11,8 +26,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    Ng2UploaderModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig, firebaseAuthConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
